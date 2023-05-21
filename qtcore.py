0dbef101688420f92d03a3946b6dfabfc97f3e7b
Julia Ajpert <jajpert@gmail.com>
2023-05-21 16:29:34 -0400

    organizando

--- qtcore.py
+++ qtcore.py
@@ -1,7 +1,5 @@
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
-from validate_docbr import *
-from validate_docbr.CPF import CPF
 from re import *
-import re
+
