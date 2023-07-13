d5ac745b354b31b6eef762b28ec1e5451695c0f2
Nathan Henrique Lima Chaves <111211074+NathanHenriqueLimaChaves@users.noreply.github.com>
2023-07-13 19:04:17 -0400

    Arrumando

--- ui_telas_abrec.py
+++ ui_telas_abrec.py
@@ -6857,7 +6857,7 @@ class Ui_MainWindow(object):
 "        colo"
                         "r: rgb(249, 217, 221);   \n"
 "}")
-        self.btn_buscar_alterar_as.setIcon(icon12)
+        self.btn_buscar_alterar_as.setIcon(icon14)
 
         self.horizontalLayout_96.addWidget(self.frame_279)
 
@@ -7343,7 +7343,7 @@ class Ui_MainWindow(object):
 "		border-bottom-right-radius: 10px;\n"
 "        color: rgb(249, 217, 221);   \n"
 "}")
-        self.btn_alterar_cep_buscar_cuidador_as.setIcon(icon12)
+        self.btn_alterar_cep_buscar_cuidador_as.setIcon(icon14)
 
         self.verticalLayout_292.addWidget(self.btn_alterar_cep_buscar_cuidador_as)
 
@@ -8253,7 +8253,7 @@ class Ui_MainWindow(object):
 "		border-bottom-right-radius: 10px;\n"
 "        color: rgb(249, 217, 221);   \n"
 "}")
-        self.btn_alterar_cep_buscar_usuario_as.setIcon(icon12)
+        self.btn_alterar_cep_buscar_usuario_as.setIcon(icon14)
 
         self.verticalLayout_294.addWidget(self.btn_alterar_cep_buscar_usuario_as)
 
@@ -10059,7 +10059,7 @@ class Ui_MainWindow(object):
 "		border-bottom-right-radius: 10px;\n"
 "        color: rgb(249, 217, 221);   \n"
 "}")
-        self.btn_alterar_cep_buscar_colaborador_as.setIcon(icon12)
+        self.btn_alterar_cep_buscar_colaborador_as.setIcon(icon14)
 
         self.verticalLayout_296.addWidget(self.btn_alterar_cep_buscar_colaborador_as)
 
