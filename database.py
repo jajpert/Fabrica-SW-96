be8d9198f4d0313cd73d92b9ce7a54aa43e99e42
Julia Ajpert <jajpert@gmail.com>
2023-07-28 19:50:27 -0400

    corrigindo main

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ class DataBase():
         self.banco = banco
 
     def connect(self):
-        ##self.conn = mysql.connector.connect(host='localhost',database='abrec2',user='root',password='3545')
+        #self.conn = mysql.connector.connect(host='localhost',database='abrec',user='root',password='3545')
         self.conn = mysql.connector.connect(host='192.168.22.9',database='abrec',user='fabrica',password='fabrica@2022')
         if self.conn.is_connected():
             self.cursor = self.conn.cursor()
