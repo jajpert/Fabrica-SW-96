07364df526aade6b14608f0bfc7073ef7d8d9853
Luan Victor <sonavitae@gmail.com>
2023-08-01 20:43:26 -0300

    Update database.py

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ class DataBase():
         self.banco = banco
 
     def connect(self):
-        self.conn = mysql.connector.connect(host='localhost',database='abrec',user='root',password='*Samela710*')
-        ##self.conn = mysql.connector.connect(host='192.168.22.9',database='abrec',user='fabrica',password='fabrica@2022')
+        #self.conn = mysql.connector.connect(host='localhost',database='abrec',user='root',password='*Samela710*')
+        self.conn = mysql.connector.connect(host='192.168.22.9',database='abrec',user='fabrica',password='fabrica@2022')
         if self.conn.is_connected():
             self.cursor = self.conn.cursor()
             db_info = self.conn.get_server_info()
