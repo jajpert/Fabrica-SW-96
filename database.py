b1c1527e7c0aaa3bee6e60307012e5e324366b5c
Nathan Henrique Lima Chaves <111211074+NathanHenriqueLimaChaves@users.noreply.github.com>
2023-08-30 20:19:18 -0400

    arrumando merge

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ class DataBase():
 
     def connect(self):
         
-        self.conn = mysql.connector.connect(host='localhost',database='abrec',user='root',password='3545')
+        self.conn = mysql.connector.connect(host='192.168.22.9',database='abrec',user='fabrica',password='fabrica@2022')
         if self.conn.is_connected():
             self.cursor = self.conn.cursor()
             db_info = self.conn.get_server_info()
@@ -34,6 +34,40 @@ class DataBase():
 
         finally:
             self.close_connection()
+    
+    def select_pessoa_cpf(self, cpf):
+        self.connect()
+        try:
+            self.cursor.execute(f"""
+                SELECT id_matricula, nome, telefone FROM pessoa WHERE cpf IN ({cpf});
+            """)
+            resultado1 = self.cursor.fetchall()
+            id_matricu = resultado1[0][0]
+
+            self.cursor.execute(f"""
+                SELECT local_tratamento FROM usuario WHERE id_matricula IN ({id_matricu});
+            """)
+            resultado2 = self.cursor.fetchall()
+
+            lista = []
+            for i in resultado1:
+                for n in i:
+                    lista.append(n)
+            for i in resultado2:
+                for n in i:
+                    lista.append(n)          
+            
+            #verifica os dados do select
+            #for linha in result:
+            #   print(linha)
+            return lista
+            #retorn a lista do banco para quem chamou a função
+        except Exception as err:
+            print(err)
+
+        finally:
+            self.close_connection()
+
     
     def select_colaborador(self):
         self.connect()
@@ -212,6 +246,27 @@ class DataBase():
             self.cursor.execute("""
                 SELECT id_usuario, id_matricula FROM usuario WHERE id_cuidador IS NULL ORDER BY id_usuario DESC LIMIT 10;
             """)
+            result = self.cursor.fetchall()
+            
+            #verifica os dados do select
+            #for linha in result:
+            #   print(linha)
+            
+            return result
+            #retorn a lista do banco para quem chamou a função
+        except Exception as err:
+            print(err)
+
+        finally:
+            self.close_connection()
+    
+    def select_agendamentos(self):
+        self.connect()
+        try:
+            self.cursor.execute("""
+                SELECT DATE_FORMAT(data, '%d/%m/%Y') AS data, TIME_FORMAT(hora, "%H:%i") AS hora, nome, profissional, anotacao FROM agendamento;
+            """)
+
             result = self.cursor.fetchall()
             
             #verifica os dados do select
