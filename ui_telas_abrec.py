25a893412fc6635920e20f4498304d0ef51037be
Luan Victor <106849037+LottaFeroce@users.noreply.github.com>
2023-09-28 19:45:29 -0400

    arrumada tela cursos

--- ui_telas_abrec.py
+++ ui_telas_abrec.py
@@ -8594,14 +8594,9 @@ class Ui_MainWindow(object):
         self.frame_175 = QFrame(self.frame_1721)
         self.frame_175.setObjectName(u"frame_175")
         self.frame_175.setMinimumSize(QSize(0, 0))
-        self.frame_175.setMaximumSize(QSize(400, 16777215))
+        self.frame_175.setMaximumSize(QSize(16777215, 16777215))
         self.frame_175.setFrameShape(QFrame.StyledPanel)
         self.frame_175.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_124 = QVBoxLayout(self.frame_175)
-        self.verticalLayout_124.setObjectName(u"verticalLayout_124")
-        self.verticalSpacer_30 = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.verticalLayout_124.addItem(self.verticalSpacer_30)
         self.verticalLayout_9 = QVBoxLayout(self.frame_175)
         self.verticalLayout_9.setObjectName(u"verticalLayout_9")
         self.verticalSpacer_30 = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
@@ -8613,8 +8608,10 @@ class Ui_MainWindow(object):
         self.input_id_matricula_user_participante_geral.setMaximumSize(QSize(50, 32))
         self.input_id_matricula_user_participante_geral.setStyleSheet(u"background-color: transparent;\n"
 "border-color: transparent;")
+        self.input_id_matricula_user_participante_geral.setEnabled(False)
 
         self.verticalLayout_9.addWidget(self.input_id_matricula_user_participante_geral)
+
 
         self.frame_4722 = QFrame(self.frame_175)
         self.frame_4722.setObjectName(u"frame_4722")
@@ -8626,6 +8623,7 @@ class Ui_MainWindow(object):
         self.label_cpf_pagina_participante_geral = QLabel(self.frame_4722)
         self.label_cpf_pagina_participante_geral.setObjectName(u"label_cpf_pagina_participante_geral")
         self.label_cpf_pagina_participante_geral.setFont(font)
+
 
         self.horizontalLayout_1562.addWidget(self.label_cpf_pagina_participante_geral)
 
